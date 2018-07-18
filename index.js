b52fa75e21f5bf67455ea67c8de04c57309baea8
Dan Mindru <dk83720@dhigroup.com>
2018-07-18 10:05:24 +0200

    Format with prettier

--- index.js
+++ index.js
@@ -1,63 +1,82 @@
-var path = require('path');
-var fs = require('fs');
-var mkpath = require('mkpath');
-var trx = require('./trx');
+const path = require("path");
+const fs = require("fs");
+const mkpath = require("mkpath");
+const trx = require("./trx");
 
-var VstsReporter = function(baseReporterDecorator, config, formatError) {
-    baseReporterDecorator(this);
+const VstsReporter = function(baseReporterDecorator, config, formatError) {
+  baseReporterDecorator(this);
 
-    var testResults;
-    var messages = [];
-    var outputDir = config.vstsReporter && config.vstsReporter.outputDir ? config.vstsReporter.outputDir : 'testresults';
-    var outputFile = config.vstsReporter && config.vstsReporter.outputFile ? config.vstsReporter.outputFile : 'testresults_${date}.xml';
+  const testResults;
+  const messages = [];
+  const outputDir =
+    config.vstsReporter && config.vstsReporter.outputDir
+      ? config.vstsReporter.outputDir
+      : "testresults";
+  const outputFile =
+    config.vstsReporter && config.vstsReporter.outputFile
+      ? config.vstsReporter.outputFile
+      : "testresults_${date}.xml";
 
-    this.onRunStart = function (browsers) {
-        testResults = {
-            name: path.join(outputDir, outputFile.replace('${date}', new Date().toISOString().replace(/:/g, ''))),
-            agent: {},
-            specs: []
-        };
+  this.onRunStart = function(browsers) {
+    testResults = {
+      name: path.join(
+        outputDir,
+        outputFile.replace(
+          "${date}",
+          new Date().toISOString().replace(/:/g, "")
+        )
+      ),
+      agent: {},
+      specs: []
     };
+  };
 
-    this.onBrowserStart = function (browser) {
-        testResults.agent = {
-            id: browser.id,
-            name: browser.name,
-            fullName: browser.fullName
-        };
+  this.onBrowserStart = function(browser) {
+    testResults.agent = {
+      id: browser.id,
+      name: browser.name,
+      fullName: browser.fullName
     };
+  };
 
-    this.specSuccess = this.specSkipped = this.specFailure = function (browser, result) {
-        messages.push(result);
-        var now = Date.now();
-        testResults.specs.push({
-            id: result.id,
-            suite: result.suite.length ? result.suite.join(' ') : 'Results not in a list',
-            description: result.description,
-            start: new Date(now),
-            finish: new Date(now + result.time),
-            time: result.time,
-            outcome: result.skipped ? 'NotExecuted' :
-                result.success ? 'Passed' : 'Failed',
-            message: result.log.join('\n'),
-            stackTrace: result.log.join('\n')
-        });
-    };
+  this.specSuccess = this.specSkipped = this.specFailure = function(
+    browser,
+    result
+  ) {
+    messages.push(result);
+    const now = Date.now();
+    testResults.specs.push({
+      id: result.id,
+      suite: result.suite.length
+        ? result.suite.join(" ")
+        : "Results not in a list",
+      description: result.description,
+      start: new Date(now),
+      finish: new Date(now + result.time),
+      time: result.time,
+      outcome: result.skipped
+        ? "NotExecuted"
+        : result.success
+          ? "Passed"
+          : "Failed",
+      message: result.log.join("\n"),
+      stackTrace: result.log.join("\n")
+    });
+  };
 
-    this.onBrowserComplete = function (browser) { };
+  this.onBrowserComplete = function(browser) {};
 
-    this.onRunComplete = function () { };
+  this.onRunComplete = function() {};
 
-    this.onExit = function (done) {
-        mkpath.sync(outputDir);
-        fs.writeFileSync(testResults.name, trx(testResults));
-        done();
-    };
+  this.onExit = function(done) {
+    mkpath.sync(outputDir);
+    fs.writeFileSync(testResults.name, trx(testResults));
+    done();
+  };
+};
 
-}
-
-VstsReporter.$inject = ['baseReporterDecorator', 'config', 'formatError'];
+VstsReporter.$inject = ["baseReporterDecorator", "config", "formatError"];
 
 module.exports = {
-    'reporter:vsts': ['type', VstsReporter]
+  "reporter:vsts": ["type", VstsReporter]
 };
--- trx.js
+++ trx.js
@@ -1,123 +1,143 @@
-var uuid = require('node-uuid');
-var os = require('os');
-var xmlbuilder = require('xmlbuilder');
-var moment = require('moment');
+const uuid = require('uuid/v4');
+const os = require("os");
+const xmlbuilder = require("xmlbuilder");
+const moment = require("moment");
 
-function pad(n, width, z) {
-  z = z || '0';
-  n = Math.abs(Math.floor(n)) + '';
+const pad = (n, width, z) => {
+  z = z || "0";
+  n = Math.abs(Math.floor(n)) + "";
   return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 }
 
-function toISOString (time) {
+const toISOString = (time) => {
   return moment().toISOString();
-};
+}
 
-function duration (start, finish) {
-  var diff = finish.getTime() - start.getTime();
-  return pad((diff / 1000 / 60 / 60) % 100, 2)
-        + ':' + pad((diff / 1000 / 60) % 60, 2)
-        + ':' + pad((diff / 1000) % 60, 2)
-        + '.' + pad(diff % 1000, 3) + '0000';
-};
+const duration = (start, finish) => {
+  const diff = finish.getTime() - start.getTime();
+  return (
+    pad((diff / 1000 / 60 / 60) % 100, 2) +
+    ":" +
+    pad((diff / 1000 / 60) % 60, 2) +
+    ":" +
+    pad((diff / 1000) % 60, 2) +
+    "." +
+    pad(diff % 1000, 3) +
+    "0000"
+  );
+}
 
-function escape(str) {
-  return str.replace(/&/g, '&amp;')
-              .replace(/</g, '&lt;')
-              .replace(/>/g, '&gt;')
-              .replace(/"/g, '&quot;')
-              .replace(/'/g, '&apos;');
-};
+const escape = (str) => {
+  return str
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&apos;");
+}
 
-module.exports = function (testResults) {
-    var start = Math.min.apply(null, testResults.specs.map(spec => spec.start.getTime()));
-    var finish = Math.max.apply(null, testResults.specs.map(spec => spec.finish.getTime()));
+const trx = (testResults) => {
+  const start = Math.min.apply(
+    null,
+    testResults.specs.map(spec => spec.start.getTime())
+  );
+  const finish = Math.max.apply(
+    null,
+    testResults.specs.map(spec => spec.finish.getTime())
+  );
 
-    var specs = testResults.specs.map(spec => {
-        spec.suite = escape(spec.suite);
-        spec.description = escape(spec.description);
-        return {
-            name: `${spec.suite} ${spec.description}`,
-            executionId: uuid(),
-            testId: uuid(),
-            result: spec
-        };
-    });
+  const specs = testResults.specs.map(spec => {
+    spec.suite = escape(spec.suite);
+    spec.description = escape(spec.description);
+    return {
+      name: `${spec.suite} ${spec.description}`,
+      executionId: uuid(),
+      testId: uuid(),
+      result: spec
+    };
+  });
 
-    const suites = {};
-    testResults.specs.forEach(spec => suites[spec.suite] = uuid());
+  const suites = {};
+  testResults.specs.forEach(spec => (suites[spec.suite] = uuid()));
 
-    var fullOutcome = testResults.specs.some(spec => spec.outcome == 'Failed') ? 'Failed' : 'Completed';
-    var passed = testResults.specs.filter(spec => spec.outcome == 'Passed');
-    var failed = testResults.specs.filter(spec => spec.outcome == 'Failed');
-    var executed = testResults.specs.filter(spec => spec.outcome != 'NotExecuted');
-    var notExecuted = testResults.specs.filter(spec => spec.outcome == 'NotExecuted');
+  const fullOutcome = testResults.specs.some(spec => spec.outcome == "Failed")
+    ? "Failed"
+    : "Completed";
+  const passed = testResults.specs.filter(spec => spec.outcome == "Passed");
+  const failed = testResults.specs.filter(spec => spec.outcome == "Failed");
+  const executed = testResults.specs.filter(
+    spec => spec.outcome != "NotExecuted"
+  );
+  const notExecuted = testResults.specs.filter(
+    spec => spec.outcome == "NotExecuted"
+  );
 
-    var unitTestResultsArray = specs.map(spec => {
-      var testResult = {
-        '@executionId': spec.executionId,
-        '@testId': spec.testId,
-        '@testName': spec.name,
-        '@computerName': os.hostname(),
-        '@duration': duration(spec.result.start, spec.result.finish),
-        '@startTime': toISOString(spec.result.start),
-        '@endTime': toISOString(spec.result.finish),
-        '@testType': '13cdc9d9-ddb5-4fa4-a97d-d965ccfc6d4b',
-        '@outcome': spec.result.outcome,
-        '@testListId': suites[spec.result.suite]
+  const unitTestResultsArray = specs.map(spec => {
+    const testResult = {
+      "@executionId": spec.executionId,
+      "@testId": spec.testId,
+      "@testName": spec.name,
+      "@computerName": os.hostname(),
+      "@duration": duration(spec.result.start, spec.result.finish),
+      "@startTime": toISOString(spec.result.start),
+      "@endTime": toISOString(spec.result.finish),
+      "@testType": "13cdc9d9-ddb5-4fa4-a97d-d965ccfc6d4b",
+      "@outcome": spec.result.outcome,
+      "@testListId": suites[spec.result.suite]
+    };
+    if (spec.result.outcome === "Failed") {
+      testResult.ErrorInfo = {
+        Message: escape(spec.result.message),
+        StackTrace: escape(spec.result.stackTrace)
       };
-      if(spec.result.outcome === 'Failed'){
-        testResult.ErrorInfo = {
-          Message:escape(spec.result.message),
-          StackTrace:escape(spec.result.stackTrace)
-        }
+    }
+    return testResult;
+  });
+
+  const testDefinitionsArray = specs.map(spec => {
+    return {
+      "@name": spec.name,
+      "@id": spec.testId,
+      Execution: {
+        "@id": spec.executionId
+      },
+      TestMethod: {
+        "@codeBase": testResults.name,
+        "@className": spec.result.suite,
+        "@name": spec.name
       }
-      return testResult;
-    });
+    };
+  });
 
-    var testDefinitionsArray = specs.map(spec => {
-      return {
-        '@name': spec.name,
-        '@id': spec.testId,
-        'Execution':{
-          '@id':spec.executionId
-        },
-        'TestMethod': {
-          '@codeBase': testResults.name,
-          '@className': spec.result.suite,
-          '@name': spec.name
-        }
-      };
-    });
+  const testEntryArray = specs.map(spec => {
+    return {
+      "@testId": spec.testId,
+      "@executionId": spec.executionId,
+      "@testListId": suites[spec.result.suite]
+    };
+  });
 
-    var testEntryArray = specs.map(spec => {
-      return {
-        '@testId': spec.testId,
-        '@executionId': spec.executionId,
-        '@testListId': suites[spec.result.suite]
-      };
-    });
+  const testListArray = Object.keys(suites).map(suite => {
+    return {
+      "@name": suite,
+      "@id": suites[suite]
+    };
+  });
 
-    var testListArray = Object.keys(suites).map(suite => {;
-      return {
-        '@name': suite,
-        '@id': suites[suite]
-      };
-    });
+  const skippedArray = notExecuted.map(spec => {
+    return { "#text": `Test '${spec.name}' was skipped in the test run.` };
+  });
 
-    var skippedArray = notExecuted.map(spec => {
-      return {'#text': `Test '${spec.name}' was skipped in the test run.`};
-    });
-
-    return xmlbuilder.create({
+  return xmlbuilder
+    .create({
       TestRun: {
-        '@id': uuid(),
-        '@name': testResults.name,
-        '@xmlns': 'http://microsoft.com/schemas/VisualStudio/TeamTest/2010',
+        "@id": uuid(),
+        "@name": testResults.name,
+        "@xmlns": "http://microsoft.com/schemas/VisualStudio/TeamTest/2010",
         Times: {
-          '@creation': toISOString(start),
-          '@start': toISOString(start),
-          '@finish': toISOString(finish)
+          "@creation": toISOString(start),
+          "@start": toISOString(start),
+          "@finish": toISOString(finish)
         },
         TestLists: {
           TestList: testListArray
@@ -131,13 +151,13 @@ module.exports = function (testResults) {
         Results: {
           UnitTestResult: unitTestResultsArray
         },
-        ResultSummary:{
-          '@outcome':fullOutcome,
-          Counters:{
-            '@total': testResults.specs.length,
-            '@executed': executed.length,
-            '@passed': passed.length,
-            '@failed': failed.length
+        ResultSummary: {
+          "@outcome": fullOutcome,
+          Counters: {
+            "@total": testResults.specs.length,
+            "@executed": executed.length,
+            "@passed": passed.length,
+            "@failed": failed.length
           },
           Output: {
             StdOut: skippedArray
@@ -145,6 +165,15 @@ module.exports = function (testResults) {
         }
       }
     })
-      .dec('1.0', 'UTF-8')
-      .end({pretty:true});
+    .dec("1.0", "UTF-8")
+    .end({ pretty: true });
 };
+
+
+module.exports = {
+  pad,
+  toISOString,
+  dufration,
+  escape,
+  trx
+}
